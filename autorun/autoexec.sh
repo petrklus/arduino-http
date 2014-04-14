5262ab90fa45d37961ab313f1e7fc7ff3e60a346
Petr Klus <petr@klus.co.uk>
2014-04-14 23:52:44 +0200

    autorun script

---
+++ autorun/autoexec.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# crontab -e
+# @reboot /usr/bin/screen -fa -d -m -S arduino $HOME/arduino-http/autorun/autoexec.sh
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+echo $DIR
+cd $DIR
+while :
+do
+        echo "Starting arduino listener/pusher"        
+        python ../main.py
+        sleep 10
+done
